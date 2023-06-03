e51c2801406e5b8dc189edeeb254f178e9eb86c2
Oscar Brennan <oscarbrennan@outlook.com>
2023-06-03 14:04:22 +0100

    Fix bug with example where device goes to sleep

--- examples/reading_temperature.rs
+++ examples/reading_temperature.rs
@@ -14,10 +14,14 @@ use rp_pico::hal;
 use core::time::Duration;
 use fugit::RateExtU32;
 
-use bme680::{Bme680, I2CAddress, IIRFilterSize, OversamplingSetting, PowerMode, SettingsBuilder};
+use bme680::{
+    Bme680, FieldDataCondition, I2CAddress, IIRFilterSize, OversamplingSetting, PowerMode,
+    SettingsBuilder,
+};
 
 #[entry]
 fn main() -> ! {
+    debug!("Start");
     let mut pac = pac::Peripherals::take().unwrap();
     let core = pac::CorePeripherals::take().unwrap();
 
@@ -37,9 +41,7 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    // Lets us wait for fixed periods of time
-    let mut delay = Delayer(Delay::new(core.SYST, clocks.system_clock.freq().to_Hz()));
-
+    // Wrapper to implement DelayUs for a Cortex M Delay
     struct Delayer(Delay);
 
     impl DelayUs for Delayer {
@@ -47,6 +49,8 @@ fn main() -> ! {
             self.0.delay_us(us);
         }
     }
+
+    let mut delay = Delayer(Delay::new(core.SYST, clocks.system_clock.freq().to_Hz()));
 
     // The single-cycle I/O block controls our GPIO pins
     let sio = hal::Sio::new(pac.SIO);
@@ -59,13 +63,10 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // Init I2C pins
+    // Init I2C
     let sda_pin = pins.gpio0.into_mode::<gpio::FunctionI2C>();
     let scl_pin = pins.gpio1.into_mode::<gpio::FunctionI2C>();
 
-    // Init I2C itself, using MaybeUninit to overwrite the previously
-    // uninitialized i2c_ctx variable without dropping its value
-    // (i2c_ctx definined in init local resources above)
     let i2c = I2C::i2c0(
         pac.I2C0,
         sda_pin,
@@ -75,7 +76,7 @@ fn main() -> ! {
         &clocks.system_clock,
     );
 
-    let mut dev = Bme680::init(i2c, &mut delay, I2CAddress::Primary).unwrap();
+    let mut dev = Bme680::init(i2c, &mut delay, I2CAddress::Secondary).unwrap();
 
     let settings = SettingsBuilder::new(&dev)
         .with_humidity_oversampling(OversamplingSetting::OS2x)
@@ -90,23 +91,42 @@ fn main() -> ! {
     let profile_dur = dev.get_profile_dur(&settings.0).unwrap();
     info!("Profile duration {:?}", profile_dur);
     info!("Setting sensor settings");
-    dev.set_sensor_settings(&mut delay, settings);
+    dev.set_sensor_settings(&mut delay, settings).unwrap();
     info!("Setting forced power modes");
-    dev.set_sensor_mode(&mut delay, PowerMode::ForcedMode);
 
     loop {
         delay.delay_ms(1000);
+
+        match dev.get_sensor_mode() {
+            Ok(PowerMode::ForcedMode) => {
+                info!("ON");
+            }
+            Ok(PowerMode::SleepMode) => {
+                info!("SLEEP");
+            }
+            Err(_) => {
+                error!("GETTING SENSOR MODE");
+            }
+        }
+
+        dev.set_sensor_mode(&mut delay, PowerMode::ForcedMode)
+            .unwrap();
+
         match dev.get_sensor_data(&mut delay) {
-            Ok((data, _state)) => {
+            Ok((data, state)) => {
                 info!("TEMPERATURE {}", data.temperature_celsius());
                 info!("HUMIDITY {}", data.humidity_percent());
                 info!("PRESSURE {}", data.pressure_hpa());
                 info!("GAS RESISTANCE {}", data.gas_resistance_ohm());
                 info!("GAS VALID {}", data.gas_valid());
                 info!("HEATER STABLE {}", data.heat_stable());
+                match state {
+                    FieldDataCondition::Unchanged => info!("NO NEW DATA"),
+                    FieldDataCondition::NewData => info!("NEW DATA"),
+                }
             }
-            Err(e) => {
-                info!("error");
+            Err(_) => {
+                error!("GETTING SENSOR DATA");
             }
         }
     }
